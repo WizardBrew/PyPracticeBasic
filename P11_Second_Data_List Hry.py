0b94fad4fe91358307071750574bd34325c65e32
WizardBrew <91134716+WizardBrew@users.noreply.github.com>
2023-03-10 00:25:43 +0530

    Update P11_Second_Data_List Hry.py

--- P11_Second_Data_List Hry.py
+++ P11_Second_Data_List Hry.py
@@ -1,11 +1,11 @@
-l1 = [4, 6, 8, 2]
+lists1 = [4, 6, 8, 2]
 
-print(l1[0]+ l1[1] + l1[2] + l1[3])           # Sums as required element or index
+print(lists1[0]+ lists1[1] + lists1[2] + lists1[3])           # Sums as required element or index
 # print(sum (l1))                             # Gives Sum of l1 list
 
 """
-if 2 in l1:
-    print("yes")                            # if loop will check either number exist in L1 list or
+if 2 in lists1:
+    print("yes")                            # if loop will check either number exist in Lists1 list or
 
 else:
     print("NO")
